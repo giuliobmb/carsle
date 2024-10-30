fc7f2fd2015f5d03e77defbbbf1952c1557760fb
giuliobmb <g.bombarda2@gmail.com>
2024-10-30 17:24:33 +0100

    comments2

--- lib/backend/gameservice.dart
+++ lib/backend/gameservice.dart
@@ -14,11 +14,11 @@ class GameService {
   Future<List<Car>> parseCars() async {
     final input = File(FILEPATH).openRead();
     final fields = await input
-        .transform(utf8.decoder) // Decodifica il file in formato UTF-8
-        .transform(const CsvToListConverter()) // Converte il CSV in una lista
+        .transform(utf8.decoder)
+        .transform(const CsvToListConverter()) 
         .toList();
 
-    // Rimuovi l'intestazione (se esiste) prima di mappare i dati
+
     final cars = fields.skip(1).where((row) => row.length >= 10).map((row) {
       return Car(
         key: row[0].toString(),
@@ -26,7 +26,7 @@ class GameService {
         brand: row[2].toString(),
         model: row[3].toString(),
         type: row[4].toString(),
-        seats: _parseSeatsOrDoors(row[5].toString()), // Usa la nuova funzione per i posti
+        seats: _parseSeatsOrDoors(row[5].toString()),
         doors: _parseSeatsOrDoors(row[6].toString()),
         acceleration: _parseAcceleration(row[7].toString()),
         image: row[8].toString(),
@@ -38,47 +38,42 @@ class GameService {
   }
 
   Future<void> init() async {
-    this.cars = await parseCars(); // Chiamata alla funzione asincrona
-    //print('Auto caricate: $cars');
+    this.cars = await parseCars();
   }
 
-// Funzione di utilità per convertire l'accelerazione in double, estraendo solo il numero
+
   double _parseAcceleration(String accelerationString) {
     try {
-      // Usa una espressione regolare per estrarre solo la parte numerica
+      
       final numericString = RegExp(r'\d+(\.\d+)?').stringMatch(accelerationString);
       if (numericString != null) {
-        return double.parse(numericString); // Converte la stringa numerica in double
+        return double.parse(numericString); 
       } else {
-        return 0.0; // Restituisce un valore predefinito se non ci sono numeri
+        return 0.0;
       }
     } catch (e) {
       print('Errore durante la conversione dell\'accelerazione: $e');
-      return 0.0; // Valore predefinito in caso di errore
+      return 0.0; 
     }
   }
 
   int _parseSeatsOrDoors(String value) {
     try {
       if (value.contains('-')) {
-        // Gestisci il caso di intervallo, ad esempio "3-5"
         final parts = value.split('-').map((e) => int.parse(e)).toList();
-        return 5; // Restituisce la media arrotondata
+        return 5; 
       }
       if (value.contains('2/3')) {
-        // Gestisci il caso di intervallo, ad esempio "3-5"
         final parts = value.split('/').map((e) => int.parse(e)).toList();
-        return 3; // Restituisce la media arrotondata
+        return 3; // 
       }
       if (value.contains('3/5')) {
-        // Gestisci il caso di intervallo, ad esempio "3-5"
         final parts = value.split('/').map((e) => int.parse(e)).toList();
-        return 5; // Restituisce la media arrotondata
+        return 5; 
       }
-      return int.parse(value); // Altrimenti, converte normalmente
+      return int.parse(value); 
     } catch (e) {
       print('Errore durante il parsing di $value: $e');
-      return 0; // Restituisce un valore predefinito in caso di errore
+      return 0; 
     }
   }
-}
